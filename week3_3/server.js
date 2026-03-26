abde4058205605b0f9b40c48ee8b3d9237ebf66f
Dana <danajoyreyes.gayosa@gmail.com>
2026-03-27 00:32:36 +1100

    Added Week 3 Express app

---
+++ week3_3/server.js
@@ -0,0 +1,9 @@
+var express = require("express")
+var app = express()
+var port = process.env.port || 3002;
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port,()=>{
+    console.log("App listening to: "+port)
+})
